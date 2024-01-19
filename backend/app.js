19eb91ccf4e498433432b15e81814ff5b6508615
Ampol321 <johnpaul.dellova@tup.edu.ph>
2024-01-19 12:16:45 +0800

    WHBS setup

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+app.use(cors())
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cookieParser());
+
+module.exports = app
